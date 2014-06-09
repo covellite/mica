bbd8d142ee37ad7fccb961e90c468927ffcac6aa
covellite <covellite.jp@gmail.com>
2014-06-09 19:37:07 +0900

    140608 モーダルを開く　修正1

--- script/mica.js
+++ script/mica.js
@@ -15,22 +15,29 @@
                 var modal_id = $(this).attr("href"),
                     modal_width = $(modal_id).outerWidth(),
                     modal_height = $(modal_id).outerHeight();
+
                 $('#overlay')
-                    .css({ "display": "block", "opacity": "0" })
+                    .css({
+                        "display": "block",
+                        "opacity": "0"
+                    })
                     .fadeTo(200, fo.overlay);
-                $(modal_id).css({
-                    "display": "block",
-                    "position": "fixed",
-                    "opacity": "0",
-                    "z-index": "10000",
-                    "top": o.top + "px",
-                    "left": "50%",
-                    "margin-left": -(modal_width / 2) + "px"
-                });
-                $(modal_id).fadeTo(200, 1);
+
+                $(modal_id)
+                    .css({
+                        "display": "block",
+                        "position": "fixed",
+                        "opacity": "0",
+                        "z-index": "10000",
+                        "top": fo.top + "px",
+                        "left": "50%",
+                        "margin-left": -(modal_width / 2) + "px"
+                    })
+                    .fadeTo(200, 1);
+
                 e.preventDefault();
             });
-
         });
     };
+
 }(jQuery));
