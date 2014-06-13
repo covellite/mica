52edffe8c3c6377ab2add1898636809997959756
covellite <covellite.jp@gmail.com>
2014-06-13 10:42:30 +0900

    140613 画像・ID分岐1

--- script/mica.js
+++ script/mica.js
@@ -4,7 +4,7 @@
     $.fn.mica = function (o) {
         var d = {
             top: 100,
-            overlay: 0.5,
+            overlay: 0.7,
             closeButton: null
         },
             fo = $.extend({}, d, o);
@@ -23,37 +23,76 @@
 
         return this.each(function () {
             $(this).click(function (e) {
-                var modal_id = $(this).attr("href"),
-                    modal_width = $(modal_id).outerWidth();
 
-                $('#overlay')
-                    .css({
-                        "display": "block",
-                        "opacity": "0"
-                    })
-                    .fadeTo(200, fo.overlay)
-                    .click(function () {
-                        close_modal(modal_id);
-                    });
+                if ($(this).attr("href").match(/^#.+/)) {
+                    var modal_id = $(this).attr("href"),
+                        modal_width = $(modal_id).outerWidth();
 
-                $(fo.closeButton)
-                    .click(function () {
-                        close_modal(modal_id);
-                    });
+                    $('#overlay')
+                        .css({
+                            "display": "block",
+                            "opacity": "0"
+                        })
+                        .fadeTo(200, fo.overlay)
+                        .click(function () {
+                            close_modal(modal_id);
+                        });
 
-                $(modal_id)
-                    .css({
-                        "display": "block",
-                        "position": "fixed",
-                        "opacity": "0",
-                        "z-index": "10000",
-                        "top": fo.top + "px",
-                        "left": "50%",
-                        "margin-left": -(modal_width / 2) + "px"
-                    })
-                    .fadeTo(200, 1);
+                    $(fo.closeButton)
+                        .click(function () {
+                            close_modal(modal_id);
+                        });
 
-                e.preventDefault();
+                    $(modal_id)
+                        .css({
+                            "display": "block",
+                            "position": "fixed",
+                            "opacity": "0",
+                            "z-index": "10000",
+                            "top": fo.top + "px",
+                            "left": "50%",
+                            "margin-left": -(modal_width / 2) + "px"
+                        })
+                        .fadeTo(200, 1);
+
+                    e.preventDefault();
+
+                } else {
+                    if ($("#modal").size() == 0) {
+                        $('body').append('<div id="modal"></div>');
+                    }
+
+                    var modal_id = '#modal';
+
+                    $(modal_id).append('<img src="' + $(this).attr("href") + '" alt="" />');
+
+                    $('#overlay')
+                        .css({
+                            "display": "block",
+                            "opacity": "0"
+                        })
+                        .fadeTo(200, fo.overlay)
+                        .click(function () {
+                            close_modal(modal_id);
+                        });
+
+                    $(fo.closeButton)
+                        .click(function () {
+                            close_modal(modal_id);
+                        });
+
+                    $(modal_id)
+                        .css({
+                            "position": "fixed",
+                            "opacity": "0",
+                            "z-index": "10000",
+                            "top": fo.top + "px",
+                            "left": "50%"
+                        })
+                        .fadeTo(200, 1);
+
+                    e.preventDefault();
+                }
             });
         });
     };
